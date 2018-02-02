8eb143ee1700666deb81131bc7e2cc7cf1613028
Rhys <rbrooker105@gmail.com>
2018-02-02 20:07:43 +1100

    complete casesar cypher exercise

---
+++ caesar.js
@@ -0,0 +1,10 @@
+const caesar = string => string.replace(/([a-z])/ig, ( char => {
+  let charNumber = char.charCodeAt(0)
+  charNumber === 122 //'z'
+    ? charNumber = 97 // 'Z'
+    : charNumber === 90 //'z' => 'a'
+      ? charNumber = 65 //'Z' => 'A'
+      : charNumber += 1
+    return String.fromCharCode(charNumber)
+  }) );
+module.exports = caesar;
